9cb4f0556f4f506758fd9319f6cfdea8f320a74e
Jam Castro <jam.castro36@gmail.com>
2022-06-17 00:59:58 +0200

    primer comit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent{
+  heroes: string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor']
+  heroeborrado: string = '';
+  borrarHeroe(){
+    this.heroeborrado = this.heroes.shift() || '';
+ }
+}
